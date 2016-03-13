aa5c35a264c431605879599cc780d0f579f8c18a
lispincsclothing <scullingnull+1@gmail.com>
2016-03-12 23:35:32 -0800

    Moved anonymous function to onRequest and logged Request Received

--- TheNodeBeginnerBook/server.js
+++ TheNodeBeginnerBook/server.js
@@ -1,7 +1,18 @@
 var http = require("http");
 
-http.createServer(function(request, response) {
+// http.createServer(function(request, response) {
+//   response.writeHead(200, {"Content-Type":"text/plain"});
+//   response.write("Hello World");
+//   response.end();
+// }).listen(8888);
+
+function onRequest(request, response) {
+  console.log("Request Received");
   response.writeHead(200, {"Content-Type":"text/plain"});
   response.write("Hello World");
   response.end();
-}).listen(8888);
+}
+
+http.createServer(onRequest).listen(8888);
+
+console.log("Server has started");
