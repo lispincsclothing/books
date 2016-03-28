470eb003878138b0641e514cb6e651293ed3bfc5
lispincsclothing <scullingnull+1@gmail.com>
2016-03-28 11:40:19 -0700

    Non-blocking function using exec, does not return results of ls

--- TheNodeBeginnerBook/requestHandlers.js
+++ TheNodeBeginnerBook/requestHandlers.js
@@ -1,12 +1,20 @@
+var exec = require("child_process").exec;
+
 function start() {
   console.log("Request handler 'start' was called.");
+  var content = "empty";
 
-  function sleep(milliSeconds) {
-    var startTime = new Date().getTime();
-    while (new Date().getTime() < startTime + milliSeconds);
-  }
-  sleep(10000);
-  return "Hello Start";
+  // function sleep(milliSeconds) {
+  //   var startTime = new Date().getTime();
+  //   while (new Date().getTime() < startTime + milliSeconds);
+  // }
+  // sleep(10000);
+
+  exec("ls -lah", function(error, stdout, stderr) {
+    content = stdout;
+  });
+
+  return content;
 }
 
 function upload() {
