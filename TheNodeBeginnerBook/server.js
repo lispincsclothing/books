af399e626a3d34049e581193f0846f50ee400121
lispincsclothing <scullingnull+1@gmail.com>
2016-03-28 11:17:53 -0700

    Created non-blocking example

--- TheNodeBeginnerBook/server.js
+++ TheNodeBeginnerBook/server.js
@@ -6,10 +6,11 @@ function start(route, handle) {
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
 
-    route(handle, pathname);
-
     response.writeHead(200, {"Content-Type":"text/plain"});
-    response.write("Hello World");
+
+    var content = route(handle, pathname);
+    console.log(content);
+    response.write(content);
     response.end();
   }
   http.createServer(onRequest).listen(8888);
